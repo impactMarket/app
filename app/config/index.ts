8a0753312f9937921b32aaa5de1193d94d559f3f
Bernardo Vieira <bernardo@impactmarket.com>
2022-02-09 16:30:24 +0000

    fix ci

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,15 +1,36 @@
+/* eslint-disable no-process-env */
 import { cleanEnv, str } from 'envalid';
 
-// eslint-disable-next-line no-process-env
-const env = cleanEnv(process.env, {
+// ATTENTION!
+// this is relevant for CI bundlesize step only. Bundlesize needs to run a build
+// which uses the NODE_ENV=production.
+// So this is the default. If CI_BUNDLESIZE is undefined, then it's replaced.
+let env = cleanEnv(process.env, {
     NEXT_PUBLIC_BASE_API_URL: str({
-        devDefault: 'https://impactmarket-api-staging.herokuapp.com'
+        default: 'https://impactmarket-api-staging.herokuapp.com'
     }),
     NEXT_PUBLIC_NETWORK_JSON_RPC: str({
-        devDefault: 'https://alfajores-forno.celo-testnet.org'
+        default: 'https://alfajores-forno.celo-testnet.org'
     }),
-    NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging'] })
+    NODE_ENV: str({
+        choices: ['development', 'test', 'production', 'staging']
+    })
 });
+
+// Used in everything else besided `yarn bundlesize`
+if (process.env.CI_BUNDLESIZE === undefined) {
+    env = env = cleanEnv(process.env, {
+        NEXT_PUBLIC_BASE_API_URL: str({
+            devDefault: 'https://impactmarket-api-staging.herokuapp.com'
+        }),
+        NEXT_PUBLIC_NETWORK_JSON_RPC: str({
+            devDefault: 'https://alfajores-forno.celo-testnet.org'
+        }),
+        NODE_ENV: str({
+            choices: ['development', 'test', 'production', 'staging']
+        })
+    });
+}
 
 const config = {
     /**
