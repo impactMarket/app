3abca7ad853db075fc9c27b3630f7721cf4eed03
Paulo Soares <paulo.soares@uphold.com>
2022-03-24 13:00:49 +0000

    Add community rules modal

---
+++ src/modals/CommunityRules.tsx
@@ -0,0 +1,41 @@
+import { Box, Button, Col, Display, ModalWrapper, Row, Text, useModal } from '@impact-market/ui';
+import { usePrismicData } from '../libs/Prismic/components/PrismicDataProvider';
+import Image from '../libs/Prismic/components/Image';
+import React from 'react';
+import RichText from '../libs/Prismic/components/RichText';
+
+const Rules = () => {
+    const { handleClose } = useModal();
+    const { extractFromModals } = usePrismicData({ list: true });
+
+    const { buttonLabel, content, items, title } = extractFromModals('communityRules') as any;
+
+    return (
+        <ModalWrapper maxW={78.25} padding={1.5} w="100%">
+            <Display medium>
+                {title}
+            </Display>
+            <RichText content={content} g500 mt={0.5} small />
+            <Box mt={2}>
+                <Row colSpan={2}>
+                    {items.map(({ icon, title, text }: any, index: number) => (
+                        <Col colSize={{ md: 6, xs: 12 }} key={index}>
+                            <Image alt={`${title} icon`} {...icon} h={2.5} w={2.5} />
+                            <Text g900 medium mt={1.25}>
+                                {title}
+                            </Text>
+                            <RichText content={text} g500 mt={0.5} small />
+                        </Col>
+                    ))}
+                </Row>
+            </Box>
+            <Box fLayout="end" mt={2} right w="100%">
+                <Button fluid="xs" onClick={handleClose}>
+                    {buttonLabel}
+                </Button>
+            </Box>
+        </ModalWrapper>
+    )
+}
+
+export default Rules;
