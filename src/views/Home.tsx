39e9ae7b59d7dffd21ed2b4565a0cee7a15bc9a8
ruifsa <ruifsa4@gmail.com>
2022-04-22 16:24:23 +0100

    Fixes in Beneficiary page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,23 @@
 import { Display, ViewContainer } from '@impact-market/ui';
+import { selectCurrentUser } from '../state/slices/auth';
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
+import { userBeneficiary } from '../utils/userTypes';
 import React from 'react';
 
 const Home: React.FC<{ isLoading?: boolean }> = props => {
     const { isLoading } = props;
+
+    const auth = useSelector(selectCurrentUser);
+    const router = useRouter();
+
+    // If the User is a Beneficiary, send to the Beneficiary page
+    // TODO: Check if this verification is supposed to stay here 
+    if(auth?.user?.type?.includes(userBeneficiary)) {
+        router.push('/beneficiary');
+
+        return null;
+    }
 
     return (
         <ViewContainer isLoading={isLoading}>
