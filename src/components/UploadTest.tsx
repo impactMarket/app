ec2bc3f6089887138b9f8d68a557ada800ae0c3c
ruifsa <ruifsa4@gmail.com>
2022-05-03 11:30:17 +0100

    Fix bug in mobile react-dropzone

--- src/components/UploadTest.tsx
+++ src/components/UploadTest.tsx
@@ -1,11 +1,12 @@
 import { Box } from '@impact-market/ui';
 import { useDropzone } from 'react-dropzone';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 // eslint-disable-next-line react/display-name
 const InputUpload = React.forwardRef((props: any) => {
     const { children, handleFiles, wrapperProps, ...dropzoneOptions } = props;
 
+    const ref = useRef();
     const { acceptedFiles, getRootProps, getInputProps, open } = useDropzone({
         // Disable click and keydown behavior
         noClick: true,
@@ -20,7 +21,7 @@ const InputUpload = React.forwardRef((props: any) => {
     return (
         <a {...wrapperProps} {...getRootProps()} onClick={open} style={{ border: '1px solid #000000' }}>
             {!!children && <Box mt={0.75}>{children}</Box>}
-            <input {...getInputProps()} />
+            <input {...getInputProps()} ref={ref}/>
         </a>
     );
 });
