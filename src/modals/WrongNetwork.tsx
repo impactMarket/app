99ceb562ad105c26a4f86cac814b297e8538fd28
ruifsa <ruifsa4@gmail.com>
2022-06-09 14:31:07 +0100

    Correct design for Wrong Network modal

--- src/modals/WrongNetwork.tsx
+++ src/modals/WrongNetwork.tsx
@@ -1,12 +1,15 @@
 import { Box, CircledIcon, ModalWrapper, Text, useModal } from '@impact-market/ui';
-// import { usePrismicData } from '../libs/Prismic/components/PrismicDataProvider';
+import { usePrismicData } from '../libs/Prismic/components/PrismicDataProvider';
 import React, { useEffect } from 'react';
+import RichText from '../libs/Prismic/components/RichText';
 import useWallet from '../hooks/useWallet';
 
 const WrongNetwork = () => {
     const { handleClose } = useModal();
     const { wrongNetwork } = useWallet();
-    // const { extractFromModals } = usePrismicData();
+    
+    const { extractFromModals } = usePrismicData();
+    const { description, title } = extractFromModals('wrongNetwork') as any;
 
     useEffect(() => {   
         if(!wrongNetwork) {
@@ -14,16 +17,12 @@ const WrongNetwork = () => {
         }
     }, [wrongNetwork]);
 
-    // TODO: load real info from Prismic
-    // const { buttonLabel, content, title } = extractFromModals('welcomeBeneficiary') as any;
-
-    // TODO: missing design for this
-
     return (
-        <ModalWrapper maxW={25} padding={1.5} w="100%">
-            <Box column fLayout="center" flex>
-                <CircledIcon icon="sad" large />
-                <Text mt={1}>You`re in the wrong network!</Text>
+        <ModalWrapper maxW={30} padding={1.5} w="100%">
+            <Box column fLayout="start" flex>
+                <CircledIcon error icon="alertCircle" large />
+                <Text g900 large medium mt={1.25}>{title}</Text>
+                <RichText content={description} g500 mt={0.5} small />
             </Box>
         </ModalWrapper>
     )
