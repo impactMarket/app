2f4c09fe5d063030fbff013970f00f790f246447
Miguel Pinto <miguel.pinto@codepoint.pt>
2022-06-15 16:07:48 +0100

    Proposals page dogfooding fixes

--- src/views/Proposals/AddCommunityPage.tsx
+++ src/views/Proposals/AddCommunityPage.tsx
@@ -7,18 +7,35 @@ import {
 import Community from './Community';
 import React, { useEffect, useState } from 'react';
 import String from '../../libs/Prismic/components/String';
+import useFilters from '../../hooks/useFilters';
 
 const AddCommunityPage = ({ setRequestsCount, requestsCount }: any) => {
     const [isLoading, setIsLoading] = useState(false);
     const [communities, setCommunities] = useState<PendingCommunities>();
     const [getPendingCommunities] = useGetPendingCommunitiesMutation();
     const limit = 10;
+    const { update, getByKey } = useFilters();
+    const [changed, setChanged] = useState<Date>(new Date());
+    const [ready, setReady] = useState(false);
     
     // Pagination
     const [offset, setItemOffset] = useState(0);
     const [totalCount, setTotalCount] = useState(0);
     const [currentPage, setCurrentPage] = useState(0);
     const pageCount = Math.ceil(totalCount / limit);
+
+    // On page load, check if there's a page or orderBy in the url and save it to state
+    useEffect(() => {
+        if(!!getByKey('page')) {
+            const page = getByKey('page') as any;
+
+            setItemOffset((page - 1) * limit);
+            setChanged(new Date());
+            setCurrentPage(page - 1);
+        }
+
+        setReady(true);
+    }, []);
 
     useEffect(() => {
         const getCommunitiesMethod = async () => {
@@ -40,7 +57,7 @@ const AddCommunityPage = ({ setRequestsCount, requestsCount }: any) => {
         };
 
         getCommunitiesMethod();
-    }, [offset]);
+    }, [offset, ready, changed]);
 
     const handlePageClick = (event: any, direction?: number) => {
         if (event.selected >= 0) {
@@ -48,18 +65,21 @@ const AddCommunityPage = ({ setRequestsCount, requestsCount }: any) => {
 
             setItemOffset(newOffset);
             setCurrentPage(event.selected);
+            update('page', event.selected + 1);
         } else if (direction === 1 && currentPage > 0) {
             const newPage = currentPage - 1;
             const newOffset = (newPage * limit) % totalCount;
 
             setItemOffset(newOffset);
             setCurrentPage(newPage);
+            update('page', newPage + 1);
         } else if (direction === 2 && currentPage < pageCount - 1) {
             const newPage = currentPage + 1;
             const newOffset = (newPage * limit) % totalCount;
 
             setItemOffset(newOffset);
             setCurrentPage(newPage);
+            update('page', newPage + 1);
         }
     };
 
