1e447c4089d1fe39553f456665d09ece7ad55ae9
ruifsa <ruifsa4@gmail.com>
2022-06-22 11:46:53 +0100

    Improvements in Add Community page (#153)

--- src/views/AddCommunity/ContractForm.tsx
+++ src/views/AddCommunity/ContractForm.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-nested-ternary */
-import { Alert, Box, Card, Col, Divider, Row, Text, Toggle } from '@impact-market/ui';
-import { getExpectedUBIDuration } from '../../utils/communities';
+import { Box, Card, Col, Divider, Row, Text } from '@impact-market/ui';
+// import { getExpectedUBIDuration } from '../../utils/communities';
+import { convertCurrency, getCurrencySymbol } from '../../utils/currencies';
 import { usePrismicData } from '../../libs/Prismic/components/PrismicDataProvider';
 import { useWatch } from "react-hook-form";
 import Input from '../../components/Input';
@@ -9,14 +10,16 @@ import RichText from '../../libs/Prismic/components/RichText';
 import Select from '../../components/Select';
 import useTranslations from '../../libs/Prismic/hooks/useTranslations';
 
-const ContractForm: React.FC<{ control: any, currency: string, errors: any, isLoading: boolean }> = props => {
-    const { control, currency, errors, isLoading } = props;
-    const [privateCommunity, setPrivateCommunity] = useState(false);
-    const [expectedUBIDuration, setExpectedUBIDuration] = useState('');
+const ContractForm: React.FC<{ control: any, currency: string, errors: any, isLoading: boolean, rates: any }> = props => {
+    const { control, currency, errors, isLoading, rates } = props;
+    const [claimAmountSuffix, setClaimAmountSuffix] = useState('');
+    const [maxClaimSuffix, setMaxClaimSuffix] = useState('');
+    // const [privateCommunity, setPrivateCommunity] = useState(false);
+    // const [expectedUBIDuration, setExpectedUBIDuration] = useState('');
 
     const { t } = useTranslations();
     const { extractFromView } = usePrismicData();
-    const { contractAlert, contractDescription, contractFirstText, contractPrivateDescription, contractPrivateTitle, contractSecondText, contractTitle } = extractFromView('formSections') as any;
+    const { contractDescription, contractFirstText, contractSecondText, contractTitle } = extractFromView('formSections') as any;
 
     // TODO: add texts to Prismic
     const baseIntervals = [
@@ -24,49 +27,77 @@ const ContractForm: React.FC<{ control: any, currency: string, errors: any, isLo
         { label: "Weekly", value: 'week' }
     ];
 
-    const togglePrivateCommunity = () => {
-        if(privateCommunity) {
-            setPrivateCommunity(false);
-        }
-        else {
-            setPrivateCommunity(true);
-        }
-    }
+    // TODO: Private Community - commented by Bernado request for now
+    // const togglePrivateCommunity = () => {
+    //     if(privateCommunity) {
+    //         setPrivateCommunity(false);
+    //     }
+    //     else {
+    //         setPrivateCommunity(true);
+    //     }
+    // }
 
+    // TODO: Expected UBI duration - commented by Bernado request for now (until we have a way to calculate this without errors)
     const claimAmountWatch = useWatch({ control, name: 'claimAmount' });
     const maxClaimWatch = useWatch({ control, name: 'maxClaim' });
-    const baseIntervalWatch = useWatch({ control, name: 'baseInterval' });
-    const incrementIntervalWatch = useWatch({ control, name: 'incrementInterval' });
+    // const baseIntervalWatch = useWatch({ control, name: 'baseInterval' });
+    // const incrementIntervalWatch = useWatch({ control, name: 'incrementInterval' });
         
+    // useEffect(() => {
+    //     let remaining = '';
+
+    //     if(claimAmountWatch && maxClaimWatch && baseIntervalWatch && incrementIntervalWatch) {
+    //         const { days, hours, minutes, months, years } = getExpectedUBIDuration(claimAmountWatch, maxClaimWatch, baseIntervalWatch, incrementIntervalWatch);
+
+    //         if (years > 0) {
+    //             remaining = `${years} ${t('years').toLowerCase()} `;
+    //         }
+    //         if (months > 0) {
+    //             remaining = `${remaining}${months} ${t('months').toLowerCase()} `;
+    //         }
+    //         if (days > 0) {
+    //             remaining = `${remaining}${days} ${t('days').toLowerCase()} `;
+    //         }
+    //         if (hours > 0) {
+    //             remaining = `${remaining}${hours} ${t('hours').toLowerCase()} `;
+    //         }
+    //         if (minutes > 0) {
+    //             remaining = `${remaining}${minutes} ${t('minutes').toLowerCase()} `;
+    //         }
+
+    //         if(remaining) {
+    //             remaining = remaining.slice(0, -1);
+    //         }
+    //     }
+
+    //     setExpectedUBIDuration(remaining);
+    // }, [claimAmountWatch, maxClaimWatch, baseIntervalWatch, incrementIntervalWatch]);
+
     useEffect(() => {
-        let remaining = '';
+        let claimAmountText = '';
 
-        if(claimAmountWatch && maxClaimWatch && baseIntervalWatch && incrementIntervalWatch) {
-            const { days, hours, minutes, months, years } = getExpectedUBIDuration(claimAmountWatch, maxClaimWatch, baseIntervalWatch, incrementIntervalWatch);
-
-            if (years > 0) {
-                remaining = `${years} ${t('years').toLowerCase()} `;
-            }
-            if (months > 0) {
-                remaining = `${remaining}${months} ${t('months').toLowerCase()} `;
-            }
-            if (days > 0) {
-                remaining = `${remaining}${days} ${t('days').toLowerCase()} `;
-            }
-            if (hours > 0) {
-                remaining = `${remaining}${hours} ${t('hours').toLowerCase()} `;
-            }
-            if (minutes > 0) {
-                remaining = `${remaining}${minutes} ${t('minutes').toLowerCase()} `;
-            }
-
-            if(remaining) {
-                remaining = remaining.slice(0, -1);
-            }
+        if(parseFloat(claimAmountWatch) > 0 && currency !== 'USD') {
+            const claimAmount = convertCurrency(parseFloat(claimAmountWatch), rates, 'USD', currency);
+            
+            // TODO: use currencyFormat here (after fixing the hook error returned because of the selectRates inside the function)
+            claimAmountText = `~ ${getCurrencySymbol(currency)} ${claimAmount.toFixed(2)}`;
         }
 
-        setExpectedUBIDuration(remaining);
-    }, [claimAmountWatch, maxClaimWatch, baseIntervalWatch, incrementIntervalWatch]);
+        setClaimAmountSuffix(claimAmountText);
+    }, [claimAmountWatch]);
+
+    useEffect(() => {
+        let maxClaimText = '';
+
+        if(parseFloat(maxClaimWatch) > 0 && currency !== 'USD') {
+            const maxClaim = convertCurrency(parseFloat(maxClaimWatch), rates, 'USD', currency);
+            
+            // TODO: use currencyFormat here (after fixing the hook error returned because of the selectRates inside the function)
+            maxClaimText = `~ ${getCurrencySymbol(currency)} ${maxClaim.toFixed(2)}`;
+        }
+
+        setMaxClaimSuffix(maxClaimText);
+    }, [maxClaimWatch]);
 
     return (
         <Row>
@@ -77,14 +108,15 @@ const ContractForm: React.FC<{ control: any, currency: string, errors: any, isLo
             </Col>
             <Col colSize={{ sm: 8, xs: 12 }} pb={1.25} pt={{ sm: 1.25, xs: 0 }}>
                 <Card padding={1.5}>
-                    {expectedUBIDuration &&
+                    {/* TODO: Expected UBI duration - commented by Bernado request for now (until we have a way to calculate this without errors) */}
+                    {/* {expectedUBIDuration &&
                         <Alert 
                             icon="alertCircle" 
                             info 
                             mb={1.5}
                             title={<RichText content={contractAlert} variables={{ value: expectedUBIDuration }} />}
                         />
-                    }
+                    } */}
                     <RichText content={contractFirstText} g600 small />
                     <RichText content={contractSecondText} g600 mt={2} small />
                     <Box fLayout="start" fWrap="wrap" flex mt={1.5}>
@@ -104,7 +136,8 @@ const ContractForm: React.FC<{ control: any, currency: string, errors: any, isLo
                                 label={t('amountPerClaim')}
                                 name="claimAmount"
                                 onKeyDown={(e: any) => (e.key === 'e' || e.key === '-') && e.preventDefault()}
-                                suffix={currency}
+                                prefix="$"
+                                suffix={claimAmountSuffix}
                                 type="number"
                                 withError={!!errors?.claimAmount}
                             />
@@ -125,7 +158,8 @@ const ContractForm: React.FC<{ control: any, currency: string, errors: any, isLo
                                 label={t('maxClaim')}
                                 name="maxClaim"
                                 onKeyDown={(e: any) => (e.key === 'e' || e.key === '-') && e.preventDefault()}
-                                suffix={currency}
+                                prefix="$"
+                                suffix={maxClaimSuffix}
                                 type="number"
                                 withError={!!errors?.maxClaim}
                             />
@@ -164,7 +198,8 @@ const ContractForm: React.FC<{ control: any, currency: string, errors: any, isLo
                             />
                         </Box>
                     </Box>
-                    <Divider margin="1.5 0" />
+                    { /* TODO: Private Community - commented by Bernado request for now */ }
+                    {/* <Divider margin="1.5 0" />
                     <Box fLayout="start" flex>
                         <Box pr={0.5}>
                             <Toggle 
@@ -173,12 +208,11 @@ const ContractForm: React.FC<{ control: any, currency: string, errors: any, isLo
                                 onChange={togglePrivateCommunity}
                             />
                         </Box>
-                        { /* TODO: Check with Bernardo how to do this part, it's the same as in the Mobile app? */ }
                         <Box w="100%">
                             <Text g700 medium small>{contractPrivateTitle}</Text>
                             <RichText content={contractPrivateDescription} g500 small />
                         </Box>
-                    </Box>
+                    </Box> */}
                 </Card>
             </Col>
         </Row>
