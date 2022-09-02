006deee14808c612ce71baf81630312850838664
Bernardo Vieira <bernardo@impactmarket.com>
2022-09-02 16:16:52 +0100

    allow to use other rpc

--- src/helpers/index.ts
+++
@@ -1,6 +0,0 @@
-import { JsonRpcProvider } from '@ethersproject/providers';
-import config from '../../config';
-
-const provider = new JsonRpcProvider(config.networkRpcUrl);
-
-export { provider };
