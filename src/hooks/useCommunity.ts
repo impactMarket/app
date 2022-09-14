52b440ce8b9a89660fc912f061abfef72d8523a6
Hugo Marques <hugomarquesdev@MacBook-Pro-de-Hugo.local>
2022-09-14 18:20:39 +0100

    Pending Community Page

---
+++ src/hooks/useCommunity.ts
@@ -0,0 +1,16 @@
+import useSWR from 'swr';
+
+export default function useCommunity(address: string, fetcher?: any) {
+    const { data, mutate, error } = useSWR(
+        `/communities/${address}`,
+        fetcher
+    );
+
+    const loadingCommunity = !data && !error;
+
+    return {
+        community: data?.data,
+        loadingCommunity,
+        mutate
+    };
+}
