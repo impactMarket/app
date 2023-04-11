dc3d4dd1dd980c7ee82212ffc6ac38795e5f5669
Hugo Marques <62065869+hugomarquesdev@users.noreply.github.com>
2023-04-11 08:11:27 +0100

    Stories Slideshow Fix (#439)
    Co-authored-by: Hugo Marques <hugomarquesdev@MacBook-Pro-de-Hugo.local>

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,13 +1,13 @@
-import { Img } from '@impact-market/ui';
-import { NextButton, Pagination, PrevButton } from "./SliderButtons";
+import { Icon, Img, colors } from '@impact-market/ui';
+import { Pagination } from './SliderButtons';
 import { getImage } from '../../utils/images';
 import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import useEmblaCarousel, { EmblaOptionsType } from 'embla-carousel-react'
+import useEmblaCarousel from 'embla-carousel-react'
+
 
 type PropType = {
-    options?: EmblaOptionsType
-    slides: any
+    slides: number[];
 }
 
 const Slider = styled.div`
@@ -26,20 +26,50 @@ const Slider = styled.div`
     }
 `;
 
+const StyledArrows = styled.div`
+    .embla__arrow{
+        background-color: ${colors.n01};
+        border-radius: 8px;
+        border: 1px solid ${colors.g300};
+        box-shadow: 0px 1px 2px rgb(16 24 40 / 5%);
+        cursor: pointer;
+        padding: 0.5rem;
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+
+        :disabled{
+            cursor: unset;
+            opacity: 0.3;
+        }
+    }
+    .embla__next{
+        right: 1rem;
+    }
+    .embla__prev{
+        left: 1rem;
+    }
+`;
+
 const EmblaCarousel = (props: PropType) => {
     const { slides } = props
     
-    const [viewportRef, embla] = useEmblaCarousel({ skipSnaps: false });
+    const [viewportRef, embla] = useEmblaCarousel({     
+        align: "start",
+        inViewThreshold: 0.7, 
+        loop: false,
+        skipSnaps: false,
+    });
     const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
     const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
     const [selectedSlide, setSelectedSlide] = useState(0)
 
-    const scrollPrev = useCallback(() => embla.scrollPrev(), [embla]);
-    const scrollNext = useCallback(() => embla.scrollNext(), [embla]);
+    const scrollPrev = useCallback(() => embla && embla.scrollPrev(), [embla]);
+    const scrollNext = useCallback(() => embla && embla.scrollNext(), [embla]);
 
     const onSelect = useCallback(() => {
         if (!embla) return;
-        setPrevBtnEnabled(embla.canScrollPrev());
+        setPrevBtnEnabled(embla.canScrollPrev())
         setNextBtnEnabled(embla.canScrollNext());
         setSelectedSlide(embla.selectedScrollSnap());
     }, [embla, selectedSlide]);
@@ -48,19 +78,42 @@ const EmblaCarousel = (props: PropType) => {
         if (!embla) return;
         onSelect();
         embla.on("select", onSelect);
+        embla.on('reInit', onSelect);
     }, [embla, onSelect]);
+
+    const PrevButton = ({ enabled, onClick }: any) => (
+        <StyledArrows>
+            <button className="embla__prev embla__arrow" disabled={!enabled} onClick={onClick}>
+                <Icon
+                    g700 
+                    icon="arrowLeft"
+                />
+            </button>
+        </StyledArrows>
+    );
+    
+    const NextButton = ({ enabled, onClick }: any) => (
+        <StyledArrows>
+            <button className="embla__next embla__arrow" disabled={!enabled} onClick={onClick}> 
+                <Icon
+                    g700 
+                    icon="arrowRight"  
+                />
+            </button>
+        </StyledArrows>
+    );
 
     return (
         <Slider className="embla">
             <div className="embla__viewport" ref={viewportRef}>
                 <div className="embla__container">
-                    {slides?.map((slide: string, key: number) => (
+                    {slides?.map((slide, key) => (
                         <div className="embla__slide" key={key}>
                             <Img 
                                 alt=""
                                 maxH="100%"
                                 maxW="100%"
-                                url={getImage({filePath: slide, fit: 'cover', height: 0, width: 0})}
+                                url={getImage({filePath: slide, fit: 'cover', height: 0, width: 0} as any)}
                             />
                         </div>
                     ))}
