1541214cdc132ac8020c512d4c6faeff1bb116ec
Marco Aurelio Salazar <90633967+marcusats@users.noreply.github.com>
2023-06-15 15:16:04 -0400

    CircleCI fix-2

--- src/views/MicroCredit/LoanRejected.tsx
+++ src/views/MicroCredit/LoanRejected.tsx
@@ -33,18 +33,18 @@ const LoanRejected = (props: any) => {
                 </Box>
 
                 <Box
-                    flex
                     fLayout="start"
+                    flex
                     fDirection={{ sm: 'column', xs: 'row' }}
                     order={{ sm: 1, xs: 0 }}
                     ml={{ sm: '2rem', xs: 0 }}
                     mb={{ sm: 0, xs: '1rem' }}
                     style={{
+                        backgroundColor: colors.g100,
                         borderRadius: '0.5rem',
                         flexBasis: '65%',
-                        backgroundColor: colors.g100,
-                        padding: '2rem',
-                        justifyContent: 'space-evenly'
+                        justifyContent: 'space-evenly',
+                        padding: '2rem'
                     }}
                 >
                     <Box>
