07e1c3c5056355c2a51ce593d70254d4a0913018
Marco Aurelio Salazar <90633967+marcusats@users.noreply.github.com>
2023-06-15 15:11:59 -0400

    CircleCI fix-1

--- src/views/MicroCredit/LoanRejected.tsx
+++ src/views/MicroCredit/LoanRejected.tsx
@@ -1,6 +1,6 @@
+import { Box, colors } from '@impact-market/ui';
 import Image from '../../libs/Prismic/components/Image';
 import RichText from '../../libs/Prismic/components/RichText';
-import { Box, colors  } from '@impact-market/ui';
 
 const LoanRejected = (props: any) => {
     const { data } = props;
@@ -18,9 +18,9 @@ const LoanRejected = (props: any) => {
         <Box fLayout="start" flex fDirection={{ sm: 'row', xs: 'column' }}>
             <Box flex fDirection={{ sm: 'row', xs: 'column' }}>
                 <Box
-                    style={{ flexBasis: '60%' }}
                     center
                     order={{ sm: 0, xs: 1 }}
+                    style={{ flexBasis: '60%' }}
                 >
                     <Image {...rejectedLoanImage} radius={0.5} w="95%" mb={1} />
                     <RichText content={rejectedLoanTitle} g800 medium />
@@ -33,19 +33,19 @@ const LoanRejected = (props: any) => {
                 </Box>
 
                 <Box
+                    flex
+                    fLayout="start"
+                    fDirection={{ sm: 'column', xs: 'row' }}
+                    order={{ sm: 1, xs: 0 }}
+                    ml={{ sm: '2rem', xs: 0 }}
+                    mb={{ sm: 0, xs: '1rem' }}
                     style={{
-                        backgroundColor: colors.g100,
-                        flexBasis: '65%',
                         borderRadius: '0.5rem',
+                        flexBasis: '65%',
+                        backgroundColor: colors.g100,
                         padding: '2rem',
                         justifyContent: 'space-evenly'
                     }}
-                    ml={{ sm: '2rem', xs: 0 }}
-                    mb={{ sm: 0, xs: '1rem' }}
-                    fLayout="start"
-                    flex
-                    fDirection={{ sm: 'column', xs: 'row' }}
-                    order={{ sm: 1, xs: 0 }}
                 >
                     <Box>
                         <RichText
