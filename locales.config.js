ad779374ccd9f398b4ab2afc0f185588b8619082
Paulo Sousa <paulo.86.sousa@gmail.com>
2023-10-13 17:09:46 +0100

    Add Luganda language to the locales

--- locales.config.js
+++ locales.config.js
@@ -9,7 +9,8 @@ const langConfig = [
     },
     { code: 'pt-BR', label: 'Português', shortCode: 'pt', dateFnsCode: 'ptBR' },
     { code: 'fr-FR', label: 'Français', shortCode: 'fr', dateFnsCode: 'fr' },
-    { code: 'es-ES', label: 'Español', shortCode: 'es', dateFnsCode: 'es' }
+    { code: 'es-ES', label: 'Español', shortCode: 'es', dateFnsCode: 'es' },
+    { code: 'lg-LG', label: 'Luganda', shortCode: 'lg', dateFnsCode: 'lg' }
 ];
 
 module.exports = langConfig;
