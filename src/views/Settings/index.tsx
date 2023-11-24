a8853dd34479fbd7c0049df10d07da4a6f390261
Hugo Marques <hugomarquesdev@MacBook-Pro-de-Hugo.local>
2023-11-24 11:14:16 +0000

    Feature: JWT Updates [PUT]

--- src/views/Settings/index.tsx
+++ src/views/Settings/index.tsx
@@ -37,8 +37,6 @@ const Settings: React.FC<{ isLoading?: boolean }> = (props) => {
                 ...data
             }).unwrap();
 
-            console.log('Result: ', result?.token);
-
             if (result) {
                 dispatch(setUser({ user: { ...result } }));
 
